eb72bbe34572dac699cea9b6b4dec1d134684ab4
yunshin <2younsin@gmail.com>
2024-04-25 15:12:21 +0900

    chore : template 수정

--- Jeff/baekjoon-input-template.js
+++ Jeff/baekjoon-input-template.js
@@ -6,10 +6,9 @@ const readline = require('readline').createInterface({
 let input = [];
 
 readline.on('line', function(line) {
-  input = line.split(' ').map(el => parseInt(el));
-}).on('close', function(){
-/*
-  솔루션 작성
-*/
+  input.push(line)
+}).on('close', function () {
+  /** 솔루션 작성 */
+  console.log(input)
   process.exit();
 });
