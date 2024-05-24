a5ba296bbc165227fa7eff49d90fe12327ce1e3c
snkchan <144839872+snkchan@users.noreply.github.com>
2024-05-24 21:13:24 +0900

    feat : 백준 11729 해결..

--- Chan/7주차/backjoon-11729.js
+++ Chan/7주차/backjoon-11729.js
@@ -1,15 +1,41 @@
-const readline = require('readline').createInterface({
+const { join } = require("path")
+
+const readline = require("readline").createInterface({
   input: process.stdin,
   output: process.stdout,
-});
+})
 
-let input = [];
+let input = []
 
-readline.on('line', function(line) {
-  input = line.split(' ').map(el => parseInt(el));
-}).on('close', function(){
-/*
-  솔루션 작성
-*/
-  process.exit();
-});
+readline
+  .on("line", function (line) {
+    input.push(+line)
+  })
+  .on("close", function () {
+    const [N] = input
+    let count = 0
+    let answer = []
+    const record = (from, to) => {
+      answer.push([from, to])
+      count++
+    }
+
+    function Hanoi(num, from, other, to) {
+      if (num === 0) {
+        return
+      } else {
+        Hanoi(num - 1, from, to, other)
+
+        record(from, to)
+
+        Hanoi(num - 1, other, from, to)
+      }
+    }
+
+    Hanoi(N, 1, 2, 3)
+
+    console.log(count)
+
+    console.log(answer.map((element) => element.join(" ")).join("\n"))
+    process.exit()
+  })
